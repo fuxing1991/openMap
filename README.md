3a5bfe224384165f042af400fed7071900027836
Fu xin <493000217@qq.com>
2020-09-24 11:26:19 +0800

    updated

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# fx-openMap
+### 使用说明
+本插件适合用于H5，APP,小程序开发时唤起手机安装的地图APP进行导航；因为微信公众号有自己的SDK，可自行到微信公众平台查看
+(使用过程中遇到问题可以加我扣扣：493000217咨询)
+
+### 使用方式
+
+```
+import Map from '@/js_sdk/fx-openMap/openMap.js'
+Map.openMap(latitude, longitude, name,type)
+
+```
+
+### 属性说明:
+
+
+| 参数            | 说明                    | 类型 |
+| --------------- | ------------------------ | ---- |
+| latitude        | 纬度（默认GCJ-02坐标系）                   | Float  |
+| longitude  | 经度（默认GCJ-02坐标系）                   | Float   |
+| name      | 地图标注名称                 | String  |
+| type      | 地图坐标系类型【“gcj02(默认)”、“wgs84”、“bd09”】              | String  |
+
---
+++ pages/index/index.vue
@@ -0,0 +1,43 @@
+<template>
+	<view class="content">
+		<view class="text-area" @tap="openMap">
+			导航
+		</view>
+	</view>
+</template>
+
+<script>
+	import Map from '@/js_sdk/fx-openMap/openMap.js'
+	export default {
+		data() {
+			return {
+				img:""
+			}
+		},
+		onReady() {
+			
+		},
+		methods: {
+			openMap(){
+				Map.openMap(39.91526781462406, 116.40393509115786, "北京天安门","bd09")
+			}
+		}
+	}
+</script>
+
+<style lang="less">
+	page{
+		background-color: #F5F5F5;
+	}
+	.content {
+		width: 100%;
+		.text-area {
+			height: 90rpx;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			background-color: #fff;
+		}
+	}
+	.charts{width: 750upx; height:500upx;background-color: #FFFFFF;}
+</style>
